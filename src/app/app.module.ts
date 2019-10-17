09b083e8dccc3fbabcf321f44640cf062889357f
Fahad Azhar <fahad.azhar@tenpearls.com>
2019-10-17 11:17:30 +0500

    NPM fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes } from '@angular/router';
+
 import {
   RouterModule,
   PreloadAllModules
@@ -14,24 +16,36 @@ import { AppComponent } from './app.component';
 import { MeetupCardComponent } from './shared/meetup-card/meetup-card.component';
 import { IntroCardComponent } from './shared/intro-card/intro-card.component';
 import { ConferenceCardComponent } from './shared/conference-card/conference-card.component';
-import { ProjectCardComponent } from './shared/project-card/project-card.component';
-import { SocialCardComponent } from './shared/social-card/social-card.component';
-import { NavigationComponent } from './shared/navigation/navigation.component';
 import { MainBannerComponent } from './shared/main-banner/main-banner.component';
-import { PakistanCardComponent } from './shared/pakistan-card/pakistan-card.component';
 import { BigCardComponent } from './shared/big-card/big-card.component';
-import { SmallCardComponent } from './shared/small-card/small-card.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ContactusCardComponent } from './shared/contactus-card/contactus-card.component';
-import { PartnersComponent } from './shared/partners/partners.component';
 import { NoContentComponent } from './no-content';
 import { HomeComponent } from './home';
 import { ContactusComponent } from './contactus';
 import { JoinComponent } from './join';
 
 import { ContactusService } from './services/contactus.service';
+import { MeetupsModule } from './creatives/creatives.module';
+import { ConferencesModule } from './conferences/conferences.module';
+import { ProjectsModule } from './projects/projects.module';
+import { PressKitModule } from './presskit/presskit.module';
+import { ProjectCardComponent } from './shared/project-card/project-card.component';
+import { SocialCardComponent } from './shared/social-card/social-card.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { PakistanCardComponent } from './shared/pakistan-card/pakistan-card.component';
+import { SmallCardComponent } from './shared/small-card/small-card.component';
+import { PartnersComponent } from './shared/partners/partners.component';
 
-import { ROUTES } from './app.route';
+const ROUTES: Routes = [
+  { path: '',      component: HomeComponent },
+  { path: 'home',  component: HomeComponent },
+  { path: 'meetups', component: MeetupsModule },
+  { path: 'conferences', component: ConferencesModule},
+  { path: 'projects', component: ProjectsModule},
+  { path: 'presskit', component: PressKitModule},
+  { path: '**',    component: NoContentComponent },
+]
 
 @NgModule({
   declarations: [
